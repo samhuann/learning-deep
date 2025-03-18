2bcd3c611d5046b055f75aac814d6f380a51e59f
Samuel Huang <shuan143@jhu.edu>
2025-03-18 10:06:04 -0400

    implement max pooling

--- First CNN/cnn.py
+++ First CNN/cnn.py
@@ -1,12 +1,15 @@
 import mnist
 from conv import Conv3x3
 from loader import load_mnist_images, load_mnist_labels
+from maxpool import MaxPool2
 
 train_images = load_mnist_images("mnist_data/train-images.idx3-ubyte") #load training images
 train_labels = load_mnist_labels("mnist_data/train-labels.idx1-ubyte") #load training labels
 
 conv = Conv3x3(8) #use 8 filters
+pool = MaxPool2()
 
 output = conv.forward(train_images[0]) #pass first image thru layer=
+output = pool.forward(output)
 print(output.shape)
 
